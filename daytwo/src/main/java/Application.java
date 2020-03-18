15c0024c8401fc4a1a46dbd8d2d04f41e5e5b5e1
Alex Kobchenko <kobchenko@arrival.com>
2020-03-18 23:57:59 +0300

    Add some code of Map using

--- daytwo/src/main/java/Application.java
+++ daytwo/src/main/java/Application.java
@@ -1,14 +1,17 @@
+import sun.jvm.hotspot.utilities.AssertionFailure;
+
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
-import java.util.Queue;
-import java.util.Random;
+import java.util.Map;
+import java.util.Set;
 import java.util.stream.Stream;
 
 public class Application {
 
     public static void main(String[] args) {
 
-        int i = 0;
+        /*int i = 0;
         boolean doNext = true;
 
         // Выход через невыполнение условия doNext
@@ -122,11 +125,80 @@ public class Application {
 
         for (int j = 0; j < 10; j++) {
             System.out.println(Roles.of(j % 3)); // Остаток от деления - %
+        }*/
+
+        //        Integer[] initial =  {1,2,3,6,4,5};
+
+        //Arrange
+        Map<Integer, String> map = new HashMap<>();
+
+        map.put(1, "open bill");
+        map.put(2, "close bill");
+        map.put(3, "get credit");
+        map.put(4, "close credit");
+        map.put(5, "partial close");
+        map.put(6, "fail payment");
+
+        Set<Integer> possibleStatuses = map.keySet();
+        createRecordsWithStatuses(possibleStatuses);
+
+        // Act
+        List<String> result = getRecordsFromUI();
+
+        // Assert
+        List<String> expected = new ArrayList<>();
+        for (int j = 0; j < possibleStatuses.size(); j++) {
+            Integer initVal = (Integer) possibleStatuses.toArray()[j];
+
+            if (map.containsKey(initVal)) {
+                String mappedVal = map.get(initVal);
+
+                expected.add(mappedVal);
+            } else {
+                expected.add("Unknown key " + initVal);
+            }
         }
 
+        System.out.println(expected);
+        System.out.println(result);
 
-        List list = new ArrayList();
+        expected.removeAll(result);
+        if(expected.size()!=0){
+            //ERROR
+            System.out.println(expected);
+            throw new AssertionFailure();
+        }
+        /*
+        String[] mapped = new String[initial.length];
 
+        for (int j = 0; j < initial.length; j++) {
+            Integer initVal = initial[j];
+            if(map.containsKey(initVal)){
+                String mappedVal = map.get(initVal);
+                mapped[j] = mappedVal;
+            }else {
+                mapped[j] = "Unknown key " + initVal;
+            }
+        }*/
+
+
+    }
+
+    private static List<String> getRecordsFromUI() {
+        List<String> result = new ArrayList<>();
+        result.add("open bill");
+        result.add("close bill");
+        result.add("get credit");
+        result.add("close credit");
+        result.add("partial close");
+        result.add("fail payment");
+        return result;
+    }
+
+    private static void createRecordsWithStatuses(Set<Integer> possibleStatuses) {
+        // method zaglushka (mock)
+        // uslovno dlya kajdogo statusa
+        // sozdaet v baze dannih zapis s takim statusom
     }
 
     static void addDefaultRole() {
